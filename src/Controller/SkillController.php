e143c2bfb77a694fec78d91cbcc2f9e8323ce9f2
chalet <r.bardouxx@hotmail.fr>
2020-01-31 11:30:32 +0100

    yes

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/skill")
+ * @Route("admin/skill")
  */
 class SkillController extends AbstractController
 {
